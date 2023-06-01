85c24f9f2867985ed0e6bde823671d33d5780aaf
FurBilici <79761548+FurBilici@users.noreply.github.com>
2023-06-01 15:07:46 +0300

    Add project files.

---
+++ Kubadabad.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KubadabadVT.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace KubadabadVT.DataAccess.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<AçmaListesi> AçmaListesis { get; set; }
+        public DbSet<AçmaRapor> AçmaRapors { get; set; }
+        public DbSet<Buluntular> Buluntulars { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
